a8c47cc58ca8b4a8b8a87393efb398de8a02f3dd
JLopez <Juan.canchila@gmail.com>
2024-10-09 09:31:37 -0500

    update barrios

--- src/app/pages/splashcreen/splashcreen.page.ts
+++ src/app/pages/splashcreen/splashcreen.page.ts
@@ -8,33 +8,13 @@ import { Router } from '@angular/router';
 })
 export class SplashcreenPage implements OnInit {
 
-  // Variables para controlar el progreso de la barra y el temporizador
-  public progress = 0; // Este puede ser utilizado más adelante para mostrar el progreso visualmente
-  public timer = 0; // Temporizador para la barra de progreso
-
   constructor(private router: Router) {}
-
-  // Método para gestionar la barra de progreso y la navegación
-  barTimer() {
-    // Intervalo para aumentar el valor del temporizador
-    let interval = setInterval(() => {
-      this.timer += 0.07; // Aumenta el temporizador en 0.07 cada 400ms
-
-      // Elimina la condición de navegación aquí
-      if (this.timer >= 1) {
-        clearInterval(interval); // Limpia el intervalo si el temporizador alcanza 1
-      }
-    }, 400); // El intervalo se ejecuta cada 400ms
-
-    // Navega a la página de login después de 10 segundos
-    setTimeout(() => {
-      clearInterval(interval); // Asegúrate de limpiar el intervalo
-      this.router.navigate(['/login']); // Navega a la página de login
-    }, 10000); // 10000 milisegundos (10 segundos)
-  }
 
   // Método que se ejecuta al inicializar el componente
   ngOnInit() {
-    this.barTimer(); // Llama al método para iniciar el temporizador
+    // Navega a la página de login después de 4 segundos
+    setTimeout(() => {
+      this.router.navigate(['/login']);
+    }, 4000); // 4000 milisegundos (4 segundos)
   }
 }
