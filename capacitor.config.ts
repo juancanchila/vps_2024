cda27a967c92f8f9b70910b6b6449f19c929ac6c
juancanchila <juan.canchila@gmail.com>
2025-07-02 17:08:06 -0500

    sync

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.vapaesa',
-  appName: 'vapaesa',
+  appName: 'vapaesaapp', // ✅ CAMBIADO para evitar conflicto
   webDir: 'www',
   bundledWebRuntime: false,
   plugins: {
